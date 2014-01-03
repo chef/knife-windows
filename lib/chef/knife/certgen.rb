eaf45da3056bbcddbfa3227e53076c4911086fc5
Mukta Aphale <Mukta.Aphale@Clogeny.com>
2014-01-03 16:50:18 +0530

    Changed the licence

--- lib/chef/knife/certgen.rb
+++ lib/chef/knife/certgen.rb
@@ -1,7 +1,5 @@
-#
-#
-# Author:: Seth Chisamore (<schisamo@opscode.com>)
-# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# Author:: Mukta Aphale (<mukta.aphale@clogeny.com>)
+# Copyright:: Copyright (c) 2014 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
