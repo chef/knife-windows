a21ad9554f252e7586c6ae028f3d3bcd7cb000d7
Mukta Aphale <mukta.aphale@clogeny.com>
2014-08-20 11:53:15 +0530

    Added new winrm commands and rspec tests for the new commands

---
+++ spec/unit/knife/winrm_server_certgen_spec.rb
@@ -0,0 +1,62 @@
+#
+# Author:: Mukta Aphale <mukta.aphale@clogeny.com>
+# Copyright:: Copyright (c) 2014 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+require 'chef/knife/winrm_server_certgen'
+require 'openssl'
+
+describe Chef::Knife::WinrmServerCertgen do
+  before(:all) do
+    @certgen = Chef::Knife::WinrmServerCertgen.new
+  end
+
+  it "generates RSA key pair" do
+    @certgen.config[:key_length] = 2048
+    key = @certgen.generate_keypair
+    key.should be_instance_of OpenSSL::PKey::RSA
+  end
+
+  it "generates X509 certificate" do
+    @certgen.config[:domain] = "test.com"
+    @certgen.config[:cert_validity] = "24"
+    key = @certgen.generate_keypair
+    certificate = @certgen.generate_certificate key
+    certificate.should be_instance_of OpenSSL::X509::Certificate
+  end
+
+  it "writes certificate to file" do
+    File.should_receive(:open).exactly(3).times
+    cert = double(OpenSSL::X509::Certificate.new)
+    key = double(OpenSSL::PKey::RSA.new)
+    @certgen.config[:cert_passphrase] = "password"
+    OpenSSL::PKCS12.should_receive(:create).with("password", "winrmcert", key, cert)
+    @certgen.write_certificate_to_file cert, "test", key
+  end
+
+  it "creates certificate" do
+    @certgen.config[:output_file] = 'winrmcert'
+    @certgen.should_receive(:generate_keypair)
+    @certgen.should_receive(:generate_certificate)
+    @certgen.should_receive(:write_certificate_to_file)
+    @certgen.ui.should_receive(:info).with("Generated Certificates:\n PKCS12 FORMAT: winrmcert.pfx\n BASE64 ENCODED: winrmcert.der\n REQUIRED FOR CLIENT: winrmcert.pem")
+    @certgen.thumbprint = "TEST_THUMBPRINT"
+    @certgen.ui.should_receive(:info).with("Certificate Thumbprint: TEST_THUMBPRINT")
+    @certgen.run
+  end
+
+end  
