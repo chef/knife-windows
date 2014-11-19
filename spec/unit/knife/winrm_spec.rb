2960b2a60808e6d334be911e766bf8de2e1501a3
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-11-19 20:51:42 +0530

    Changes to add winrm_authentication_protocol option

--- spec/unit/knife/winrm_spec.rb
+++ spec/unit/knife/winrm_spec.rb
@@ -246,6 +246,7 @@ describe Chef::Knife::Winrm do
         context "validate sspinegotiate transport option" do
           before do
             Chef::Config[:knife] = {:winrm_transport => 'plaintext'}
+            Chef::Config[:knife][:winrm_authentication_protocol] = "negotiate"
             allow(@winrm).to receive(:relay_winrm_command).and_return(0)
           end
 
@@ -266,35 +267,54 @@ describe Chef::Knife::Winrm do
             exit_code = @winrm.run
           end
 
-          context "when domain name not given" do
-            it "should skip winrm monkey patch for windows" do
-              @winrm.config[:winrm_user] = "testuser"
-              allow(Chef::Platform).to receive(:windows?).and_return(true)
-              expect(@winrm).to_not receive(:require).with('winrm-s')
-
-              exit_code = @winrm.run
-            end
-          end
-
-          context "when local domain name given"  do
-            it "should use the winrm monkey patch for windows" do
-              @winrm.config[:winrm_user] = ".\\testuser"
-              allow(Chef::Platform).to receive(:windows?).and_return(true)
-              allow(@winrm.ui).to receive(:warn)
-              expect(@winrm).to receive(:require).with('winrm-s')
-
-              exit_code = @winrm.run
-            end
-          end
-
           it "should not have winrm opts transport set to sspinegotiate for unix" do
             allow(Chef::Platform).to receive(:windows?).and_return(false)
 
-            expect(@winrm).to receive(:create_winrm_session).with({:user=>"testuser", :password=>"testpassword", :port=>nil, :operation_timeout=>1800, :no_ssl_peer_verification => false, :basic_auth_only=>true, :transport=>:plaintext, :disable_sspi=>true, :host=>"localhost"})
+            expect(@winrm).to receive(:create_winrm_session).with({:user=>"testuser", :password=>"testpassword", :port=>nil, :operation_timeout=>1800, :basic_auth_only=>true, :transport=>:plaintext, :disable_sspi=>true, :host=>"localhost"})
             exit_code = @winrm.run
           end
         end
 
+        context "when winrm_authentication_protocol specified" do
+          before do
+            Chef::Config[:knife] = {:winrm_transport => :plaintext}
+            allow(@winrm).to receive(:relay_winrm_command).and_return(0)
+          end
+
+          it "raise an error if value is other than [basic, negotiate, kerberos]" do
+            Chef::Config[:knife][:winrm_authentication_protocol] = "invalid"
+            @winrm.config[:winrm_user] = "domain\\testuser"
+            allow(Chef::Platform).to receive(:windows?).and_return(true)
+            expect(@winrm.ui).to receive(:error)
+            expect(@winrm).to receive(:exit)
+            exit_code = @winrm.run
+          end
+
+          it "skip winrm monkey patch for 'basic' authentication" do
+            Chef::Config[:knife][:winrm_authentication_protocol] = "basic"
+            @winrm.config[:winrm_user] = "domain\\testuser"
+            allow(Chef::Platform).to receive(:windows?).and_return(true)
+            expect(@winrm).to_not receive(:require).with('winrm-s')
+            exit_code = @winrm.run
+          end
+
+          it "skip winrm monkey patch for 'kerberos' authentication" do
+            Chef::Config[:knife][:winrm_authentication_protocol] = "kerberos"
+            @winrm.config[:winrm_user] = "domain\\testuser"
+            allow(Chef::Platform).to receive(:windows?).and_return(true)
+            expect(@winrm).to_not receive(:require).with('winrm-s')
+            exit_code = @winrm.run
+          end
+
+          it "raise error on linux for 'negotiate' authentication" do
+            Chef::Config[:knife][:winrm_authentication_protocol] = "negotiate"
+            @winrm.config[:winrm_user] = "domain\\testuser"
+            allow(Chef::Platform).to receive(:windows?).and_return(false)
+            expect(@winrm).to_not receive(:require).with('winrm-s')
+            expect(@winrm.ui).to receive(:error)
+            exit_code = @winrm.run
+          end
+        end
       end
     end
   end
