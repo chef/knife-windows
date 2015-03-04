d65826e8d238092d89281343ad8fa3bc57fcf490
prabhu-das <prabhu.das@clogeny.com>
2015-03-04 14:58:27 +0530

    updated hostname default option to blank for knife window cert generate

--- lib/chef/knife/windows_cert_generate.rb
+++ lib/chef/knife/windows_cert_generate.rb
@@ -75,9 +75,6 @@ class Chef
       end
 
       def generate_certificate rsa_key
-        @hostname = "*"
-        @hostname = config[:hostname] if config[:hostname]
-
         #Create a self-signed X509 certificate from the rsa_key (unencrypted)
         cert = OpenSSL::X509::Certificate.new
         cert.version = 2
@@ -141,7 +138,7 @@ class Chef
           write_certificate_to_file cert, file_path, rsa_key
           ui.info "Generated Certificates:"
           ui.info "- #{filename}.pfx - PKCS12 format key pair. Contains public and private keys, can be used with an SSL server."
-          ui.info "- #{filename}.b64 - Base64 encoded PKCS12 key pair. Contains public and private keys, used by some cloud provider API's to configure SSL servers." 
+          ui.info "- #{filename}.b64 - Base64 encoded PKCS12 key pair. Contains public and private keys, used by some cloud provider API's to configure SSL servers."
           ui.info "- #{filename}.pem - Base64 encoded public certificate only. Required by the client to connect to the server."
           ui.info "Certificate Thumbprint: #{@thumbprint.to_s.upcase}"
         rescue => e
