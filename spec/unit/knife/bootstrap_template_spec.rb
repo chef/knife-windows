e508e7a920eb3f8bdb2f14eb3cd8e22eb52bab87
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-03-05 20:01:55 -0500

    Make current tests pass.

--- spec/unit/knife/bootstrap_template_spec.rb
+++ spec/unit/knife/bootstrap_template_spec.rb
@@ -28,11 +28,11 @@ describe "While Windows Bootstrapping" do
     template = bootstrap.load_template
     template_file_lines = template.split('\n')
     it "should download Platform specific MSI" do
-      download_url=template_file_lines.find {|l| l.include?("url=")}
+      download_url=template_file_lines.find {|l| l.include?("default_url = ")}
       download_url.include?("%MACHINE_OS%") && download_url.include?("%MACHINE_ARCH%")
     end
     it "should download specific version of MSI if supplied" do
-      download_url_ext= template_file_lines.find {|l| l.include?("url +=")}
+      download_url_ext= template_file_lines.find {|l| l.include?("default_url +=")}
       download_url_ext.include?("[:bootstrap_version]")
     end
   end
