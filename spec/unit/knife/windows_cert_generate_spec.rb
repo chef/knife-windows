c8185a63613e9255f24cadbbd136b97ac832c61b
Vasu1105 <vasundhara.jagdale@clogeny.com>
2015-04-24 17:48:13 +0530

    Updated specs for windows cert generate

--- spec/unit/knife/windows_cert_generate_spec.rb
+++ spec/unit/knife/windows_cert_generate_spec.rb
@@ -22,7 +22,7 @@ require 'openssl'
 
 describe Chef::Knife::WindowsCertGenerate do
   before(:all) do
-    @certgen = Chef::Knife::WindowsCertGenerate.new(["-H","abc"])
+    @certgen = Chef::Knife::WindowsCertGenerate.new(["-H","something.mydomain.com"])
   end
 
   it "generates RSA key pair" do
