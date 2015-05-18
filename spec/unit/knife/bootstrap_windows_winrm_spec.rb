1c7bc19d224896c15e66b80b0f698da243eb34dc
Vasu1105 <vasundhara.jagdale@clogeny.com>
2015-05-18 15:02:26 +0530

    Fixed validatorless bootstrap test case for chef 11

--- spec/unit/knife/bootstrap_windows_winrm_spec.rb
+++ spec/unit/knife/bootstrap_windows_winrm_spec.rb
@@ -39,7 +39,8 @@ describe Chef::Knife::BootstrapWindowsWinrm do
   let(:session) { Chef::Knife::Winrm::WinrmSession.new({ :host => 'winrm.cloudapp.net', :port => '5986', :transport => :ssl }) }
 
   let(:initial_fail_count) { 4 }
-  it 'should retry if a 401 is received from WinRM' do
+
+    it 'should retry if a 401 is received from WinRM' do
     call_result_sequence = Array.new(initial_fail_count) {lambda {raise WinRM::WinRMHTTPTransportError.new('', '401')}}
     call_result_sequence.push(0)
     allow(bootstrap).to receive(:run_command).and_return(*call_result_sequence)
@@ -61,18 +62,6 @@ describe Chef::Knife::BootstrapWindowsWinrm do
     bootstrap.send(:wait_for_remote_response, 2)
   end
 
-  it 'should have a wait timeout of 2 minutes by default' do
-    allow(bootstrap).to receive(:run_command).and_raise(WinRM::WinRMHTTPTransportError.new('','500'))
-    allow(bootstrap).to receive(:create_bootstrap_bat_command).and_raise(SystemExit)
-    expect(bootstrap).to receive(:wait_for_remote_response).with(2)
-    allow(bootstrap).to receive(:validate_name_args!).and_return(nil)
-    allow(bootstrap.client_builder).to receive(:run)
-    allow(bootstrap.client_builder).to receive(:client_path).and_return("/")
-    allow(bootstrap.ui).to receive(:info)
-    bootstrap.config[:auth_timeout] = bootstrap.options[:auth_timeout][:default]
-    expect { bootstrap.bootstrap }.to raise_error(SystemExit)
-  end
-
   it 'should keep retrying at 10s intervals if the timeout in minutes has not elapsed' do
     call_result_sequence = Array.new(initial_fail_count) {lambda {raise WinRM::WinRMHTTPTransportError.new('', '500')}}
     call_result_sequence.push(0)
@@ -84,36 +73,102 @@ describe Chef::Knife::BootstrapWindowsWinrm do
     bootstrap.send(:wait_for_remote_response, 2)
   end
 
-  it "should exit bootstrap with non-zero status if the bootstrap fails" do
-    command_status = 1
+  context "when validation_key is not present" do
 
-    #Stub out calls to create the session and just get the exit codes back
-    winrm_mock = Chef::Knife::Winrm.new
-    allow(Chef::Knife::Winrm).to receive(:new).and_return(winrm_mock)
-    allow(winrm_mock).to receive(:run).and_raise(SystemExit.new(command_status))
-    #Skip over templating stuff and checking with the remote end
-    allow(bootstrap.client_builder).to receive(:run)
-    allow(bootstrap.client_builder).to receive(:client_path).and_return("/")
-    allow(bootstrap).to receive(:create_bootstrap_bat_command)
-    allow(bootstrap).to receive(:wait_for_remote_response)
-    allow(bootstrap.ui).to receive(:info)
+    before do
+      allow(File).to receive(:exist?).with(File.expand_path(Chef::Config[:validation_key])).and_return(false)
+      bootstrap.define_singleton_method(:client_builder){nil}
+    end
 
-    expect { bootstrap.run_with_pretty_exceptions }.to raise_error(SystemExit) { |e| expect(e.status).to eq(command_status) }
+    it 'should have a wait timeout of 2 minutes by default' do
+      allow(bootstrap).to receive(:run_command).and_raise(WinRM::WinRMHTTPTransportError.new('','500'))
+      allow(bootstrap).to receive(:create_bootstrap_bat_command).and_raise(SystemExit)
+      expect(bootstrap).to receive(:wait_for_remote_response).with(2)
+      allow(bootstrap).to receive(:validate_name_args!).and_return(nil)
+      expect(bootstrap.client_builder).to receive(:run)
+      expect(bootstrap.client_builder).to receive(:client_path).and_return("/")
+      allow(bootstrap.ui).to receive(:info)
+      bootstrap.config[:auth_timeout] = bootstrap.options[:auth_timeout][:default]
+      expect { bootstrap.bootstrap }.to raise_error(SystemExit)
+    end
+
+    it "should exit bootstrap with non-zero status if the bootstrap fails" do
+      command_status = 1
+
+      #Stub out calls to create the session and just get the exit codes back
+      winrm_mock = Chef::Knife::Winrm.new
+      allow(Chef::Knife::Winrm).to receive(:new).and_return(winrm_mock)
+      allow(winrm_mock).to receive(:run).and_raise(SystemExit.new(command_status))
+      #Skip over templating stuff and checking with the remote end
+      allow(bootstrap.client_builder).to receive(:run)
+      allow(bootstrap.client_builder).to receive(:client_path).and_return("/")
+      allow(bootstrap).to receive(:create_bootstrap_bat_command)
+      allow(bootstrap).to receive(:wait_for_remote_response)
+      allow(bootstrap.ui).to receive(:info)
+
+      expect { bootstrap.run_with_pretty_exceptions }.to raise_error(SystemExit) { |e| expect(e.status).to eq(command_status) }
+    end
+
+    it 'should stop retrying if more than 2 minutes has elapsed' do
+      times = [ Time.new(2014, 4, 1, 22, 25), Time.new(2014, 4, 1, 22, 51), Time.new(2014, 4, 1, 22, 28) ]
+      allow(Time).to receive(:now).and_return(*times)
+      allow(bootstrap.client_builder).to receive(:run)
+      allow(bootstrap.client_builder).to receive(:client_path).and_return("/")
+      run_command_result = lambda {raise WinRM::WinRMHTTPTransportError, '401'}
+      allow(bootstrap).to receive(:validate_name_args!).and_return(nil)
+      allow(bootstrap).to receive(:run_command).and_return(run_command_result)
+      allow(bootstrap).to receive(:print)
+      allow(bootstrap.ui).to receive(:info)
+      allow(bootstrap.ui).to receive(:error)
+      expect(bootstrap).to receive(:run_command).exactly(1).times
+      bootstrap.config[:auth_timeout] = bootstrap.options[:auth_timeout][:default]
+      expect { bootstrap.bootstrap }.to raise_error RuntimeError
+    end
   end
 
-  it 'should stop retrying if more than 2 minutes has elapsed' do
-    times = [ Time.new(2014, 4, 1, 22, 25), Time.new(2014, 4, 1, 22, 51), Time.new(2014, 4, 1, 22, 28) ]
-    allow(Time).to receive(:now).and_return(*times)
-    allow(bootstrap.client_builder).to receive(:run)
-    allow(bootstrap.client_builder).to receive(:client_path).and_return("/")
-    run_command_result = lambda {raise WinRM::WinRMHTTPTransportError, '401'}
-    allow(bootstrap).to receive(:validate_name_args!).and_return(nil)
-    allow(bootstrap).to receive(:run_command).and_return(run_command_result)
-    allow(bootstrap).to receive(:print)
-    allow(bootstrap.ui).to receive(:info)
-    allow(bootstrap.ui).to receive(:error)
-    expect(bootstrap).to receive(:run_command).exactly(1).times
-    bootstrap.config[:auth_timeout] = bootstrap.options[:auth_timeout][:default]
-    expect { bootstrap.bootstrap }.to raise_error RuntimeError
+  context "when validation_key is present" do
+    before do
+      allow(File).to receive(:exist?).with(File.expand_path(Chef::Config[:validation_key])).and_return(true)
+    end
+
+    it 'should have a wait timeout of 2 minutes by default' do
+      allow(bootstrap).to receive(:run_command).and_raise(WinRM::WinRMHTTPTransportError.new('','500'))
+      allow(bootstrap).to receive(:create_bootstrap_bat_command).and_raise(SystemExit)
+      expect(bootstrap).to receive(:wait_for_remote_response).with(2)
+      allow(bootstrap).to receive(:validate_name_args!).and_return(nil)
+      allow(bootstrap.ui).to receive(:info)
+      bootstrap.config[:auth_timeout] = bootstrap.options[:auth_timeout][:default]
+      expect { bootstrap.bootstrap }.to raise_error(SystemExit)
+    end
+
+    it "should exit bootstrap with non-zero status if the bootstrap fails" do
+      command_status = 1
+
+      #Stub out calls to create the session and just get the exit codes back
+      winrm_mock = Chef::Knife::Winrm.new
+      allow(Chef::Knife::Winrm).to receive(:new).and_return(winrm_mock)
+      allow(winrm_mock).to receive(:run).and_raise(SystemExit.new(command_status))
+      #Skip over templating stuff and checking with the remote end
+      allow(bootstrap).to receive(:create_bootstrap_bat_command)
+      allow(bootstrap).to receive(:wait_for_remote_response)
+      allow(bootstrap.ui).to receive(:info)
+
+      expect { bootstrap.run_with_pretty_exceptions }.to raise_error(SystemExit) { |e| expect(e.status).to eq(command_status) }
+    end
+
+    it 'should stop retrying if more than 2 minutes has elapsed' do
+      times = [ Time.new(2014, 4, 1, 22, 25), Time.new(2014, 4, 1, 22, 51), Time.new(2014, 4, 1, 22, 28) ]
+      allow(Time).to receive(:now).and_return(*times)
+      run_command_result = lambda {raise WinRM::WinRMHTTPTransportError, '401'}
+      allow(bootstrap).to receive(:validate_name_args!).and_return(nil)
+      allow(bootstrap).to receive(:run_command).and_return(run_command_result)
+      allow(bootstrap).to receive(:print)
+      allow(bootstrap.ui).to receive(:info)
+      allow(bootstrap.ui).to receive(:error)
+      expect(bootstrap).to receive(:run_command).exactly(1).times
+      bootstrap.config[:auth_timeout] = bootstrap.options[:auth_timeout][:default]
+      expect { bootstrap.bootstrap }.to raise_error RuntimeError
+    end
   end
+
 end
