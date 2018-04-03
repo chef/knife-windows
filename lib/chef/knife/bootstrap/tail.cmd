e068ae18ecddadb3190cf746fb14bf8026c3590f
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-04-03 08:38:39 +0200

    Prevent running into more.com memory issue in tail.cmd

--- lib/chef/knife/bootstrap/tail.cmd
+++ lib/chef/knife/bootstrap/tail.cmd
@@ -1,5 +1,8 @@
 @echo off
 set shown=0
+REM This next line switches the codepage to prevent running into a 'Out of memory' bug
+REM in more.com on  Windows 2008 (and maybe newer) systems.
+chcp 437 > nul 2>&1
 :tail
 if NOT EXIST "%1" (
   IF NOT %shown%==0 goto :eof
