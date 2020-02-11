b9113f79870ce080ebd378e0490abba89c35c503
Tim Smith <tsmith84@gmail.com>
2020-02-11 09:03:58 -0800

    Lazy load winrm_session for speedup
    This is the longest loading knife plugin on my mac and adds .3 seconds
    to every knife command run on the system
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/knife/winrm.rb
+++ lib/chef/knife/winrm.rb
@@ -18,7 +18,6 @@
 
 require "chef/knife"
 require_relative "winrm_knife_base" # WinrmCommandSharedFunctions
-require_relative "winrm_session"
 require_relative "knife_windows_base"
 
 class Chef
@@ -32,6 +31,7 @@ class Chef
         require_relative "windows_cert_generate"
         require_relative "windows_cert_install"
         require_relative "windows_listener_create"
+        require_relative "winrm_session"
         require "readline"
         require "chef/search/query"
       end
